f4b76c55946b5aee855015481502610e39c0e5e4
Sebastian Roberts <tiansebas09@gmail.com>
2020-11-03 13:57:29 -0600

    File Browser & Ico

--- Soundboard/Soundboard/Form1.Designer.cs
+++ Soundboard/Soundboard/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playButton = new System.Windows.Forms.Button();
+            this.testButton = new System.Windows.Forms.Button();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.fileSelectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(53, 41);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(86, 41);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            // 
+            // testButton
+            // 
+            this.testButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testButton.Location = new System.Drawing.Point(72, 88);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(50, 23);
+            this.testButton.TabIndex = 1;
+            this.testButton.Text = "Test";
+            this.testButton.UseVisualStyleBackColor = true;
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(189, 53);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(235, 20);
+            this.filePath.TabIndex = 2;
+            // 
+            // fileSelectButton
+            // 
+            this.fileSelectButton.Location = new System.Drawing.Point(430, 51);
+            this.fileSelectButton.Name = "fileSelectButton";
+            this.fileSelectButton.Size = new System.Drawing.Size(75, 23);
+            this.fileSelectButton.TabIndex = 3;
+            this.fileSelectButton.Text = "Browse";
+            this.fileSelectButton.UseVisualStyleBackColor = true;
+            this.fileSelectButton.Click += new System.EventHandler(this.fileSelectButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(549, 450);
+            this.Controls.Add(this.fileSelectButton);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.testButton);
+            this.Controls.Add(this.playButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Soundboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button testButton;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Button fileSelectButton;
     }
 }
 
--- Soundboard/Soundboard/Form1.cs
+++ Soundboard/Soundboard/Form1.cs
@@ -16,5 +16,22 @@ namespace Soundboard
         {
             InitializeComponent();
         }
+
+        string audioFile;
+
+        private void fileSelectButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Video Files (*.mp3, *.mp4, *.wmv)|*.mp3; *.mp4; *.wmv";
+                ofd.FilterIndex = 1;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    filePath.Text = ofd.FileName;
+                    audioFile = ofd.FileName;
+                }
+            }
+        }
     }
 }
